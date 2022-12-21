eac5ef21dc6c91df0270d753bfc8d828b13acfed
Subhrajyoti Basu <44311167+subhrajyoti-basu@users.noreply.github.com>
2022-12-21 18:18:27 +0530

    changes in cors

--- build/index.js
+++ build/index.js
@@ -37,7 +37,13 @@ dotenv.config();
 var app = (0, _express2.default)();
 var PORT = process.env.PORT || 8080;
 
-app.use((0, _cors2.default)({ origin: '*' }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}
+
+// app.use((0, _cors2.default)({ origin: '*' }));
 // app.use((req, res, next) => {
 //     res.setHeader("Access-Control-Allow-Origin", "https://app.marketled.online");
 //     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
