2f4fe2313a899ae97ee482a451d8e6227cfe32de
Subhrajyoti Basu <44311167+subhrajyoti-basu@users.noreply.github.com>
2022-12-21 18:23:26 +0530

    fix

--- build/index.js
+++ build/index.js
@@ -41,7 +41,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-}
+})
 
 // app.use((0, _cors2.default)({ origin: '*' }));
 // app.use((req, res, next) => {
