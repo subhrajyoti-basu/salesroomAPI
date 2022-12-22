9ffcdaaf31d61217e12e19846200e91ca0949086
subhrajyoti-basu <subhrajyoti.basu@outlook.com>
2022-12-22 22:57:31 +0530

    .

--- build/index.js
+++ build/index.js
@@ -43,7 +43,7 @@ app.use(_bodyParser2.default.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
